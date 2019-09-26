d094107963eb947114228cd7ccda8532d48cb4ec
dzheng2019 <55662540+dzheng2019@users.noreply.github.com>
2019-09-26 13:49:48 -0400

    collected mal data

--- WebScrapper/parsedata.py
+++ WebScrapper/parsedata.py
@@ -1,13 +1,14 @@
 from pprint import pprint
 import json
 
-with open('twitterData.json') as f:
+with open('malData.json') as f:
     jsonData = json.load(f)
 
 count = 0
-
+total = 0
 for i in jsonData:
-    if "obama" in i['tweet'].lower():
+    total += 1
+    if  float(i['score']) >= 9.0:
         count += 1;
 
 print (count)
--- WebScrapper/webscraper.py
+++ WebScrapper/webscraper.py
@@ -2,36 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-url = 'https://myanimelist.net/topanime.php?limit=00'
+url = 'https://myanimelist.net/topanime.php?limit='
 response = requests.get(url, timeout=300)
 content = BeautifulSoup(response.content, "html.parser")
-tweetArr = []
+listArr = []
 
-print(content)
+for x in range(100):
+    url = 'https://myanimelist.net/topanime.php?limit=' + str((x*50))
+    response = requests.get(url, timeout=300)
+    content = BeautifulSoup(response.content, "html.parser")
 
+    for listing in content.find_all('tr', attrs={"class": "ranking-list"}):
+        listObject = {
+            "name": listing.find('a', attrs={"class": "hoverinfo_trigger fl-l fs14 fw-b"}).text,
+            "score": listing.find('span', attrs={"class": "text on"}).text
+        }
+        listArr.append(listObject)
 
-
-# for tweet in content.find_all('div', attrs={"class": "tweetcontainer"}):
-#     tweetObject = {
-#         "author": tweet.find('h2', attrs={"class": "author"}).text,
-#         "date": tweet.find('h5', attrs={"class": "dateTime"}).text,
-#         "tweet": tweet.find('p', attrs={"class": "content"}).text,
-#         "likes": tweet.find('p', attrs={"class": "likes"}).text,
-#         "shares": tweet.find('p', attrs={"class": "shares"}).text
-#     }
-#     tweetArr.append(tweetObject)
-#
-# with open('twitterData.json', 'w') as outfile:
-#     json.dump(tweetArr, outfile)
-
-# count = 0
-#
-# for head in content.find_all('div', attrs={"class": "_1poyrkZ7g36PawDueRza-J _11R7M_VOgKO1RJyRSRErT3  "}):
-#     #head.find('div', attrs={"class": "_2SdHzo12ISmrC8H86TgSCp _3wqmjmv3tb_k-PROt7qFZe "})
-#     print(head.find('h3', attrs={'class': "_eYtD2XCVieq6emjKBH3m"}).text)
-#
-#     if count > 100:
-#         break
-#     count+=1
-#
-# print("cat")
+with open('malData.json', 'w') as outfile:
+    json.dump(listArr, outfile)
